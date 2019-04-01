e0917aa631829e64fedb793d60d6a60296dd4d7e
Johnathon Kwisses <johnkwisses@gmail.com>
2019-04-01 12:42:42 -0600

    Final version of P3; full functionality.

---
+++ src/scripts/john.js
@@ -0,0 +1,15 @@
+function showButtons() {
+    document.getElementById("buttons").className = "visible";
+}
+
+function hideButtons() {
+    document.getElementById("buttons").className = "hide";
+}
+
+function showSummary() {
+    document.getElementById("summary").className = "visible";
+}
+
+function hideSummary() {
+    document.getElementById("summary").className = "hide";
+}
